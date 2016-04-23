a3200d49fd99c82a4f2d166c1db6abb1094a52e3
Anuj More <anujmorex@gmail.com>
2016-04-23 18:00:47 -0500

    Reorganize code into logic and utilities

--- stats/_code_tagoreweb_p1.R
+++ stats/_code_tagoreweb_p1.R
@@ -1,58 +1,11 @@
-require("RPostgreSQL")
+library(RPostgreSQL)
 library(RTextTools)
+
+# Include common utilities
+source('./util.R')
 
 setwd("~/_code/me/2obindro/stats")
 table <- "tagoreweb"
-
-connect <- function(table) {
-  drv <- dbDriver("PostgreSQL")
-  con <- dbConnect(drv, dbname = "robindro", user = "robindro")
-
-  # Early return if table does not exist
-  if (!(dbExistsTable(con, table))) {
-    stop("Table does not exist")
-  }
-  con
-}
-
-select <- function(con, column_names=c()) {
-  columns <- paste(column_names, collapse=", ")
-  if(columns == "") {
-    columns = "*"
-  }
-  dbGetQuery(con, paste("SELECT", columns,  "FROM", table))
-}
-
-process <- function(df, algos, split = 0.8) {
-  # Shuffle
-  df <- df[sample(nrow(df)),]
-  
-  print("Creating document matrix")
-  doc_matrix <- create_matrix(df$lyrics, language="english", removeNumbers=TRUE, removeSparseTerms=.998)
-  
-  print("Creating container")
-  total_rows = nrow(df)
-  split_num = round(total_rows * split)
-  container <- create_container(doc_matrix, as.numeric(factor(df$parjaay)),
-                                trainSize=1:split_num, testSize=(split_num + 1):total_rows, virgin=FALSE)
-
-  print("Training models")
-  models <- train_models(container, algorithms=algos)
-  print("Generating results")
-  results <- classify_models(container, models)
-  print("Generating analytics")
-  analytics <- create_analytics(container, results)
-}
-
-output <- function(analytics, table, folder_name = "results") {
-  summary(analytics)
-  timestamp <- as.numeric(Sys.time())  
-  file_prefix <- paste(table, "p1", timestamp, sep=".")
-  write.csv(analytics@document_summary, paste(folder_name, "/", file_prefix, ".document.csv", sep=""))
-  write.csv(analytics@algorithm_summary, paste(folder_name, "/", file_prefix, ".algorithm.csv", sep=""))
-  write.csv(analytics@ensemble_summary,  paste(folder_name, "/", file_prefix, ".ensemble.csv", sep=""))
-  write.csv(analytics@label_summary, paste(folder_name, "/", file_prefix, ".label.csv", sep=""))
-}
 
 # Get connection
 con <- connect(table)
@@ -72,8 +25,7 @@ df <- subset(df_full, parjaay %in% categories)
 
 # List of algorithms to apply
 algos <- c("MAXENT","SVM","GLMNET","TREE", "BOOSTING", "BAGGING", "SLDA")
-# algos <- c("SVM")
+# Return analytics after running the algos on th data frame
 analytics <- process(df, algos)
+# Output all the results to screen and files
 output(analytics, table)
-
-# TODO: Stemming
---
+++ stats/util.R
@@ -0,0 +1,49 @@
+connect <- function(table) {
+  drv <- dbDriver("PostgreSQL")
+  con <- dbConnect(drv, dbname = "robindro", user = "robindro")
+  
+  # Early return if table does not exist
+  if (!(dbExistsTable(con, table))) {
+    stop("Table does not exist")
+  }
+  con
+}
+
+select <- function(con, column_names=c()) {
+  columns <- paste(column_names, collapse=", ")
+  if(columns == "") {
+    columns = "*"
+  }
+  dbGetQuery(con, paste("SELECT", columns,  "FROM", table))
+}
+
+process <- function(df, algos, split = 0.8) {
+  # Shuffle
+  df <- df[sample(nrow(df)),]
+  
+  print("Creating document matrix")
+  doc_matrix <- create_matrix(df$lyrics, language="english", removeNumbers=TRUE, removeSparseTerms=.998)
+  
+  print("Creating container")
+  total_rows = nrow(df)
+  split_num = round(total_rows * split)
+  container <- create_container(doc_matrix, as.numeric(factor(df$parjaay)),
+                                trainSize=1:split_num, testSize=(split_num + 1):total_rows, virgin=FALSE)
+  
+  print("Training models")
+  models <- train_models(container, algorithms=algos)
+  print("Generating results")
+  results <- classify_models(container, models)
+  print("Generating analytics")
+  analytics <- create_analytics(container, results)
+}
+
+output <- function(analytics, table, folder_name = "results") {
+  summary(analytics)
+  timestamp <- as.numeric(Sys.time())  
+  file_prefix <- paste(table, "p1", timestamp, sep=".")
+  write.csv(analytics@document_summary, paste(folder_name, "/", file_prefix, ".document.csv", sep=""))
+  write.csv(analytics@algorithm_summary, paste(folder_name, "/", file_prefix, ".algorithm.csv", sep=""))
+  write.csv(analytics@ensemble_summary,  paste(folder_name, "/", file_prefix, ".ensemble.csv", sep=""))
+  write.csv(analytics@label_summary, paste(folder_name, "/", file_prefix, ".label.csv", sep=""))
+}
